4b2cd6bb2ecf55b01d5547fd3c418eb89cc76117
NguyenDinhSon <25023375@vnu.edu.vn>
2026-05-10 19:31:35 +0700

    Add CI workflow

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("2 + 3 = " + calc.add(2, 3));
+    }
+}
